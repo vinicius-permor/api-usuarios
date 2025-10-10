cbceafe3b27bc908c922ecd09a64a2a48446b6b7
Marcus Vinicius Pereira de Moraes <ma.vinicius.moraes@gmail.com>
2025-10-10 08:55:09 -0300

    implementando o pacote controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+	"os/user"
+
 	"github.com/gin-gonic/gin"
 
+	"vinicius-permor/apiGin/models"
 	"vinicius-permor/apiGin/services"
 )
 
@@ -14,5 +18,81 @@ func NewUserController(services *services.UserService) *UserControllers {
 	return &UserControllers{service: services}
 }
 
+func (usrControllers *UserControllers) CreateUser(c *gin.Context) {
+	var users models.Users
+	if err := c.ShouldBindJSON(&users); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	id, err := usrControllers.service.CreateUser(&users)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+
+	users.ID = int(id)
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "cliente criado com sucesso",
+		"data":    users,
+	})
+}
+
 func (usrControllers *UserControllers) ListAllUser(c *gin.Context) {
+	users, err := usrControllers.service.ListAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": users,
+	})
+}
+
+func (usrControllers *UserControllers) SearchUser(c *gin.Context) {
+	id := c.Param("id")
+	users, err := usrControllers.service.SearchUserID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": users,
+	})
+}
+
+func (usrControllers *UserControllers) UpadateUser(c *gin.Context) {
+	var users models.Users
+	id := c.Param("id")
+	if err := c.ShouldBindJSON(&users); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "usuario autalizado com sucesso",
+		"data":    id,
+	})
+}
+
+func (usrControllers *UserControllers) DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	err := usrControllers.service.DeleteUserID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "cliente foi deletado",
+	})
 }
--- services/servicesCliente.go
+++ services/servicesCliente.go
@@ -1,6 +1,11 @@
 package services
 
-import "vinicius-permor/apiGin/repositories"
+import (
+	"errors"
+
+	"vinicius-permor/apiGin/models"
+	"vinicius-permor/apiGin/repositories"
+)
 
 type UserService struct {
 	repo *repositories.UsersRepository
@@ -8,4 +13,38 @@ type UserService struct {
 
 func NewUserService(repo *repositories.UsersRepository) *UserService {
 	return &UserService{repo: repo}
+}
+
+func (s *UserService) CreateUser(user *models.Users) (int64, error) {
+	return s.repo.Create(*user)
+}
+
+func (s *UserService) SearchUserID(id string) (*models.Users, error) {
+	user, err := s.repo.SearchID(id)
+	if err != nil {
+		return nil, err
+	}
+	if user == nil {
+		return nil, errors.New("usuario nao econtrado")
+	}
+	return user, nil
+}
+
+func (s *UserService) UpdateUserID(id string, user *models.Users) error {
+	users, err := s.repo.SearchID(id)
+	if err != nil {
+		return err
+	}
+	if users == nil {
+		return errors.New("usuario nao econtrado")
+	}
+	return s.repo.UpdateID(id, user)
+}
+
+func (s *UserService) DeleteUserID(id string) error {
+	return s.repo.DeleteUser(id)
+}
+
+func (s *UserService) ListAllUsers() ([]models.Users, error) {
+	return s.repo.ListallUsers()
 }
